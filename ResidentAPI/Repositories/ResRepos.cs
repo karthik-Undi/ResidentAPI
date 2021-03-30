d423761ba79976e6d846732867c5c9d3eaf4c8e0
Karthik Venkata <Karthik.Venkata@gisbizdev.gisbiz.chennai.com>
2021-03-30 15:12:54 +0530

    Add project files.

---
+++ ResidentAPI/Repositories/ResRepos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ResidentAPI.Models;
+namespace ResidentAPI.Repositories
+{
+    public class ResRepos:IResRepos
+    {
+        private readonly CommunityGateDatabaseContext _context;
+        public ResRepos(CommunityGateDatabaseContext context)
+        {
+            this._context = context;
+        }
+        public ResRepos()
+        {
+
+        }
+
+    }
+}
